1bb0446f05488f840f0020b1539b42d30e77fff3
mimuneru <mimuneru@gmail.com>
2025-06-21 21:21:06 +0900

    Refactor ESLint configuration to simplify rules and remove TypeScript support
    - Removed TypeScript parser and related rules from `.eslintrc.js`.
    - Updated the `extends` section to only include `eslint:recommended`.
    - Disabled `no-unused-vars` and `no-console` rules for a more lenient linting approach.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,17 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint:recommended',
-    '@typescript-eslint/recommended',
-  ],
-  plugins: ['@typescript-eslint'],
   env: {
     node: true,
     es2020: true,
   },
+  extends: [
+    'eslint:recommended',
+  ],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
   rules: {
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/explicit-function-return-type': 'warn',
-    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-unused-vars': 'off',
+    'no-console': 'off',
   },
 }; 
